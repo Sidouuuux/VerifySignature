d66deb97c081663990c8c68c1bedbc5d832d83dc
Sid-Ahmed MIR <sid-ahmed@Sid-Ahmeds-iMac.local>
2023-06-05 12:16:46 +0400

    Add tests ✅

--- test/VerifySignature.test.js
+++ test/VerifySignature.test.js
@@ -7,10 +7,69 @@ describe("VerifySignature", function () {
   let addr1;
   let message = "Hello World";
   beforeEach(async function () {
-    [owner, addr1] = await ethers.getSigners();
+    const accounts = await ethers.getSigners(2);
 
     Contract = await ethers.getContractFactory("VerifySignature");
     contract = await Contract.deploy();
     await contract.deployed();
+  });
+
+  it("Check signature", async function () {
+    const accounts = await ethers.getSigners(2);
+
+    // const PRIV_KEY = "0x..."
+    // const signer = new ethers.Wallet(PRIV_KEY)
+    const signer = accounts[0];
+    const message = "Hello";
+
+    const hash = await contract.getMessageHash(message);
+    const sig = await signer.signMessage(ethers.utils.arrayify(hash));
+
+    const ethHash = await contract.getEthSignedMessageHash(hash);
+
+    console.log("signer          ", signer.address);
+    console.log("recovered signer", await contract.recoverSigner(ethHash, sig));
+
+    expect(await contract.verify(signer.address, message, sig)).to.equal(true);
+  });
+
+  it("Check incorrect signature", async function () {
+    const accounts = await ethers.getSigners(2);
+
+    // const PRIV_KEY = "0x..."
+    // const signer = new ethers.Wallet(PRIV_KEY)
+    const signer = accounts[0];
+    const message = "Hello";
+
+    const hash = await contract.getMessageHash(message);
+    const sig = await signer.signMessage(ethers.utils.arrayify(hash));
+
+    const ethHash = await contract.getEthSignedMessageHash(hash);
+
+    console.log("signer          ", signer.address);
+    console.log("recovered signer", await contract.recoverSigner(ethHash, sig));
+
+    // Incorrect message returns false
+    expect(await contract.verify(signer.address, "message", sig)).to.equal(
+      false
+    );
+  });
+  
+  it("Check signature length", async function () {
+    const accounts = await ethers.getSigners(2);
+
+    const signer = accounts[0];
+    const message = "Hello";
+
+    const hash = await contract.getMessageHash(message);
+    const sig = await signer.signMessage(ethers.utils.arrayify(hash));
+
+    // Modify the signature length to be less than 65
+    const modifiedSig = sig.slice(0, 64);
+
+    // Verify that the contract reverts with the expected error message
+    await expect(
+      contract.verify(signer.address, message, modifiedSig)
+    ).to.be.revertedWith("invalid signature length");
   });
 });
